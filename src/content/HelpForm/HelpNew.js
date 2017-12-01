f5d6dd626110235e2944ae18d024045784c9c400
Leonid Zorin <leonid9.021@gmail.com>
2017-12-01 16:05:35 +0100

    Progress

--- src/content/HelpForm/HelpNew.js
+++ src/content/HelpForm/HelpNew.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import { ButtonPanel, Button } from 'odeum-ui'
 
-import { HelpTitle } from './HelpStyles'
+import { DoublePanel, InputGreen, Panel } from './HelpStyles'
 import HelpErrors from './HelpErrors'
 
 class HelpNew extends Component {
-
 	constructor(props) {
 		super(props)
+
+		this.inputRefs = {}
 
 		this.state = {
 			locale_content: {
@@ -18,9 +19,10 @@ class HelpNew extends Component {
 					helptitle: ''
 				}
 			},
-			helpErrors: { helptitle: '' },
+			helpErrors: { locale_content: { en: { helptitle: '' } } },
 			helptitlelValid: false,
-			formValid: false
+			helpValid: false,
+			inFocus: ''
 		}
 	}
 
@@ -52,33 +54,42 @@ class HelpNew extends Component {
 		this.setState({
 			locale_content: {
 				en: {
-					helptitle: 'english'
+					helptitle: ''
 				},
 				da: {
-					helptitle: 'dansk'
+					helptitle: ''
 				}
 			}
 		})
 		this.focusHelpTitleInput()
 	}
 
-	handleChange = (e) => {
-		// console.log('Input changed', e.target.name)
-		const name = e.target.name
-		const value = e.target.value
-		/* This is an example on how to go down to this.state.locale_content.en.helptitle that might help you or make you more confused
+	handleContentChange = (e) => {
 		this.setState({
 			locale_content: {
 				...this.state.locale_content,
-				en: {
-					...this.state.en
-					[name]: value
+				[e.target.lang]: {
+					...this.state.locale_content[e.target.lang],
+					help_content: {
+						...this.state.locale_content[e.target.lang].help_content,
+						[e.target.name]: e.target.value
+					}
 				}
 			}
 		})
-		*/
-		this.setState({ [name]: value },
-			() => { this.validateField(name, value) })
+	}
+
+	handleChange = (e) => {
+		this.setState({
+			locale_content: {
+				...this.state.locale_content,
+				[e.target.lang]: {
+					...this.state.locale_content[e.target.lang],
+					[e.target.name]: e.target.value
+				}
+			}
+		
+		})
 	}
 
 	handleSubmit = (e) => {
@@ -89,12 +100,14 @@ class HelpNew extends Component {
 
 	validateField(fieldName, value) {
 		let fieldValidationErrors = this.state.helpErrors
-		let enHelptitleValid = this.state.en.helptitleValid
+		let helptitleValid = this.state.locale_content.en.helptitleValid
 
 		switch (fieldName) {
-			case 'helptitle':
-				enHelptitleValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)
-				fieldValidationErrors.helptitle = enHelptitleValid ? '' : ' is invalid'
+			case 'help_title':
+				// helptitleValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)
+				// fieldValidationErrors.helptitle = helptitleValid ? '' : ' is invalid'
+				helptitleValid = value.length >= 1
+				fieldValidationErrors.locale_content.en.helptitleValid = helptitleValid ? '' : ' is too short'
 				break
 			default:
 				break
@@ -102,15 +115,16 @@ class HelpNew extends Component {
 
 		this.setState({
 			helpErrors: fieldValidationErrors,
-			helptitleValid: enHelptitleValid,
+			helptitleValid: helptitleValid,
 		}, this.validateHelp)
 	}
 
 	validateHelp() {
-		this.setState({ helpValid: this.state.en.helptitleValid })
+		this.setState({ helpValid: this.state.en.locale_content.helptitleValid })
 	}
 
 	errorClass(error) {
+		
 		return (error.length === 0 ? '' : 'has-error')
 		// return (error.length === 0 ? false : true)
 	}
@@ -138,76 +152,50 @@ class HelpNew extends Component {
 	}
 
 	render() {
-		const { en, /* da */ } = this.state.locale_content
+		const { en, da } = this.state.locale_content
+
 		return (
 			<div>
 				<h1>Help Demo</h1>
 				<p>Please enter your help item:</p>
-				<form>
-					<div>
-						<div>English</div>
-						<HelpTitle
-							type={'helptitle'}
-							// required 
-							placeholder={'Help title'}
-							name={'helptitle'}
-							value={en.helptitle}
-							disabled={false}
-							onChange={this.handleChange}
-							innerRef={this.createRef}
-							color={!this.state.en.helptitleValid ? '#BE4F44' : undefined}
-							focusColor={!this.state.en.helptitleValid ? '#BE4F44' : undefined}
-							onFocus={this.handleFocus}
-							onBlur={this.handleChange}
-							onMouseEnter={this.handleMouse('Enter')}
-							onMouseLeave={this.handleMouse('Leave')}
-						/>
-						<div>Danish</div>
-						{/* <HelpTitle
-							type={'helptitle'}
-							// required 
-							placeholder={'Help title'}
-							name={'helptitle'}
-							value={da.helptitle}
-							disabled={false}
-							onChange={this.handleChange}
-							innerRef={this.createRef}
-							color={!this.state.helptitleValid ? '#BE4F44' : undefined}
-							focusColor={!this.state.helptitleValid ? '#BE4F44' : undefined}
-							onFocus={this.handleFocus}
-							onBlur={this.handleChange}
-							onMouseEnter={this.handleMouse('Enter')}
-							onMouseLeave={this.handleMouse('Leave')}
-						/> */}
-					</div>
-
-					<ButtonPanel justify={'left'} >
-
-						<Button
-							label={'Save'}
-							icon={'check'}
-							type={'submit'}
-							onClick={this.handleSubmit}
-							disabled={!this.state.helpValid}
-							isDisabled={!this.state.helpValid}
-							color={this.state.helpValid ? '#13A085' : ''}
-							onSubmit={this.handleOnSubmit}
-						/>
-
-						<Button
-							label={'Reset'}
-							icon={'close'}
-							type={'reset'}
-							onClick={this.handleResetInput}
-							color={'#BE4F44'}
-						/>
-
-					</ButtonPanel>
-					<div>
-						<HelpErrors helpErrors={this.state.helpErrors} />
-						{`${this.errorClass(en.helptitle)}`}{' '}
-					</div>
-				</form>
+				<DoublePanel>
+					<Panel>
+						<form>
+							<InputGreen value={en.helptitle} innerRef={this.createRef} name={'helptitle'} onChange={this.handleChange} lang={'en'} />
+						</form>
+					</Panel>
+					<Panel>
+						<form>
+							<InputGreen value={da.helptitle} name={'helptitle'} onChange={this.handleChange} lang={'da'} />
+						</form>
+					</Panel>
+				</DoublePanel>
+				<ButtonPanel justify={'left'} >
+				
+					<Button
+						label={'Save'}
+						icon={'check'}
+						type={'submit'}
+						onClick={this.handleSubmit}
+						disabled={!this.state.helpValid}
+						isDisabled={!this.state.helpValid}
+						color={this.state.helpValid ? '#13A085' : ''}
+						onSubmit={this.handleOnSubmit}
+					/>
+				
+					<Button
+						label={'Reset'}
+						icon={'close'}
+						type={'reset'}
+						onClick={this.handleResetInput}
+						color={'#BE4F44'}
+					/>
+				
+				</ButtonPanel>
+				<div>
+					<HelpErrors helpErrors={this.state.helpErrors} />
+					{`${this.errorClass(this.state.locale_content.en.helptitle)}`}{' '}
+				</div>
 			</div>
 		)
 	}
