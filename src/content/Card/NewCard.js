102a491912c54b5fafae269b9807f948c451eed6
Andrei Tudor <andrei.tudor27@gmail.com>
2018-02-22 13:19:12 +0100

    Animation

--- src/content/Card/NewCard.js
+++ src/content/Card/NewCard.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react'
-import { FormContainer, FormImg, Header, ProjectInfo, Title, ProjectInfoCategory, ButtonContainer, Button, ControlsContainer, ExpandButtonContainer } from './NewCardStyles'
+import { FormContainer, FormImg, Header, ProjectInfo, Title, ProjectInfoCategory, ButtonContainer, Button, ControlsContainer, ExpandButtonContainer, VerticalButtonContainer } from './NewCardStyles'
 import { Icon } from 'odeum-ui'
 
 export default class NewFormCard extends Component {
@@ -29,55 +29,61 @@ export default class NewFormCard extends Component {
 		const { label, date, regs, resp } = this.props
 		const { expand, horizOpen } = this.state
 		return (
-			<div style={{ display: 'flex', flexFlow: 'column nowrap', alignItems: 'center' }}>
+			<Fragment>
+				<div style={{ display: 'flex', flexFlow: 'row nowrap', margin: '10px' }}>
 
-				<FormContainer expand={expand} id={this.props.id} horizOpen={horizOpen}>
-					<FormImg>
-						<img src='' alt='projectimg' />
-					</FormImg>
-					<Header>
-						<Icon icon={'info'} iconSize={22} style={{ marginRight: '5px' }} />{label}</Header>
-					<ProjectInfo>
-						<ProjectInfoCategory>
-							<Title>Date</Title>
-							{date}
-						</ProjectInfoCategory>
-						<ProjectInfoCategory>
-							<Title>Reg.</Title>
-							{regs}
-						</ProjectInfoCategory>
-						<ProjectInfoCategory>
-							<Title>Responsible</Title>
-							{resp}
-						</ProjectInfoCategory>
-					</ProjectInfo>
-					{/* <div style={{ background: 'green', margin: 10, padding: 3 }}> progressBar here</div> */}
+					<div style={{ display: 'flex', flexFlow: 'column nowrap', alignItems: 'center' }}>
 
-					{/* {expand ? <div>Lorem Ipsum </div> : null} */}
-				</FormContainer>
-				<ControlsContainer>
-					<Fragment>
-						<ButtonContainer horizOpen={this.state.horizOpen} style={{ flexFlow: 'row nowrap' }}>
-							<Button horizOpen={this.state.horizOpen} onClick={this.handleExpand}>
-								<Icon color={'#5E5E5E'} icon={'mode_edit'} iconSize={23} />
-							</Button>
-							<Button horizOpen={this.state.horizOpen}>
-								<Icon color={'#5E5E5E'} icon={'share'} iconSize={23} />
-							</Button>
-							<Button horizOpen={this.state.horizOpen}>
-								<Icon color={'#5E5E5E'} icon={'library_add'} iconSize={23} />
-							</Button>
-						</ButtonContainer>
-						<ExpandButtonContainer horizOpen={false}>
-							<div onClick={this.expandHoriz}
-								style={{ border: 'none', backgroundColor: 'inherit', outline: 0 }}>
-								<Icon icon={'more_horiz'} iconSize={23}/>
-							</div>
-						</ExpandButtonContainer>
-					</Fragment>
-				</ControlsContainer>
+						<FormContainer expand={expand} id={this.props.id} horizOpen={horizOpen}>
+							<FormImg>
+								<img src='' alt='projectimg' />
+							</FormImg>
+							<Header>
+								<Icon icon={'info'} iconSize={22} style={{ marginRight: '5px' }} />{label}</Header>
+							<ProjectInfo>
+								<ProjectInfoCategory>
+									<Title>Date</Title>
+									{date}
+								</ProjectInfoCategory>
+								<ProjectInfoCategory>
+									<Title>Reg.</Title>
+									{regs}
+								</ProjectInfoCategory>
+								<ProjectInfoCategory>
+									<Title>Responsible</Title>
+									{resp}
+								</ProjectInfoCategory>
+							</ProjectInfo>
+							{/* <div style={{ background: 'green', margin: 10, padding: 3 }}> progressBar here</div> */}
 
-			</div>
+							{/* {expand ? <div>Lorem Ipsum </div> : null} */}
+						</FormContainer>
+						<ControlsContainer>
+							<Fragment>
+								<ButtonContainer horizOpen={this.state.horizOpen} style={{ flexFlow: 'row nowrap' }}>
+									<Button horizOpen={this.state.horizOpen} onClick={this.handleExpand}>
+										<Icon color={'#5E5E5E'} icon={'mode_edit'} iconSize={23} />
+									</Button>
+									<Button horizOpen={this.state.horizOpen}>
+										<Icon color={'#5E5E5E'} icon={'share'} iconSize={23} />
+									</Button>
+									<Button horizOpen={this.state.horizOpen}>
+										<Icon color={'#5E5E5E'} icon={'library_add'} iconSize={23} />
+									</Button>
+								</ButtonContainer>
+								<ExpandButtonContainer horizOpen={false} onClick={this.expandHoriz}>
+
+									<Icon icon={'more_horiz'} iconSize={23} />
+								</ExpandButtonContainer>
+							</Fragment>
+						</ControlsContainer>
+
+					</div>
+					<VerticalButtonContainer horizOpen={false} onClick={this.expandHoriz}>
+						<Icon icon={'more_vert'} iconSize={23} />
+					</VerticalButtonContainer>
+				</div>
+			</Fragment>
 		)
 	}
 }
--- src/content/Card/NewCardStyles.js
+++ src/content/Card/NewCardStyles.js
@@ -10,14 +10,14 @@ height:100%;
 `
 export const FormContainer = styled.div`
 border: 1px solid;
-border-bottom: 0px;
+border-right: 0px;
+border-bottom:0px;
 border-color: #e5e6e9 #dfe0e4 #d0d1d5;
 border-radius: ${p => p.horizOpen ? '10px 10px 0px 0px' : '10px'};
 min-width: ${p => p.expand ? 'calc( 100% - 20px)' : '230px'};
 min-height: 300px;
 display:flex;
 flex-flow: column nowrap;
-margin: 10px 10px 0px 10px;
 /* padding:5px; */
 overflow:hidden;
 box-shadow: 0px -2px 10px 0px rgba(0,0,0,0.2);
@@ -72,37 +72,29 @@ justify-content: space-around;
 width: 100%;
 max-width: 290px;
 /* transition: all 100ms linear; */
-margin: 10px;
-margin-top:0px;
 border-radius: ${p => p.horizOpen ? '0px 0px 10px 10px' : '0px 0px 300px 300px'};
 `
 export const ExpandButtonContainer = styled.div`
+cursor:pointer;
 align-self: bottom;
 display: flex;
 flex-flow: column nowrap;
 align-items: center;
 justify-content: space-around;
-/* padding: 5px; */
 background: #D5D5D5;
-width: ${p => p.horizOpen ? '100%' : '30%'};
+width: 40%;
 max-width: 290px;
-transition: all 100ms linear;
-/* margin: 0px 10px; */
-margin-top:0px;
-border-radius: ${p => p.horizOpen ? '0px 0px 10px 10px' : '0px 0px 300px 300px'};
-/* &:after{
-	content:'';
-	width:80%;
-	height:10px;
-	background:#D5D5D5;
-	border-radius: 0px 0px 10px 10px;
-}
-&:before{
-	background:#D5D5D5;
-	content:'';
-	height:10px;
-	width:100%;
-} */
+/* transition: all 100ms linear; */
+border-radius: ${p => p.horizOpen ? '0px 0px 10px 10px' : '0px 0px 150px 150px'};
+height: 15px;
+`
+
+export const VerticalButtonContainer = ExpandButtonContainer.extend`
+max-height:200px;
+border-radius: ${p => p.vertOpen ? '0px 0px 10px 10px' : '0px 50px 50px 0px'};
+width: 15px;
+height: 100px;
+margin-top: 40px;
 `
 
 export const ButtonContainer = styled.div`
@@ -118,13 +110,14 @@ background: #D5D5D5;
 /* width: ${p => p.horizOpen ? '100%' : '30%'}; */
 width: 100%;
 max-width: 225px;
-transition: all 400ms cubic-bezier(1, 1, 0, 1.5);
+/* transition: all 400ms cubic-bezier(1, 1, 0, 1.5); */
+transition: all 250ms cubic-bezier(.87,-.41,.19,1.44);
 border-radius: 0px 0px 10px 10px;
 overflow:hidden;
 `
 
 export const Button = styled.div`
-/* visibility: ${p => p.horizOpen ? 'visible' : 'hidden'}; */
+opacity: ${p => p.horizOpen ? '1' : '0'};
 border: 1px solid #D5D5D5;
 border-radius: 100px;
 cursor: pointer;
@@ -132,5 +125,6 @@ cursor: pointer;
 	background: #E6E6E6;
 	border: 1px solid #E6E6E6;
 }
-transition: all ${p => p.horizOpen ? '40ms' : '300ms'} cubic-bezier(1, 1, 0, 2.5);
+/* transition: all ${p => p.horizOpen ? '40ms' : '300ms'} cubic-bezier(1, 1, 0, 2.5); */
+transition: opacity 0.1s cubic-bezier(.87,-.41,.19,1.44);
 `
